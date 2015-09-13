2c74756d686a229eb6ab6549dd73705f0d0fae29
noobanalyst <noobanalyst@users.noreply.github.com>
2015-09-13 16:54:52 +0530

    Update Data_Extraction.R
    Made changes to add some new columns

--- Data_Extraction.R
+++ Data_Extraction.R
@@ -1,4 +1,6 @@
 library(XML)
+library(xlsx)
+
 yearURL <- "http://stats.espncricinfo.com/ci/engine/records/team/match_results_year.html?class=11;id=6;type=team"
 
 # Read table data from yearURL
@@ -29,28 +31,50 @@ names(yearHTML) <- yearListParse
 tempMatchList <- lapply(yearHTML, readHTMLTable)
 
 # Removing unwanted elements from this list
-matchList <- lapply(1:69, function(x) tempMatchList[[x]][[1]])
+matchList <- lapply(seq(tempMatchList), function(x) tempMatchList[[x]][[1]])
+
+# Naming the list elements Year wise
+names(matchList) <- yearListParse
 
 # Extracting URL for the specific match pages
 matchURL <- lapply(yearHTML, xpathSApply, "//a[contains(@href, '/ci/engine/match/')]/@href")
 
 # Removing element name "href"
-tempMatchURL <- lapply(1:69, function(x) unname(matchURL[[x]]))
+matchURL <- lapply(seq(matchURL), function(x) unname(matchURL[[x]]))
 
 # Removing duplicates from the list
-tempMatchURL2 <- lapply(1:69, function(x) unique(matchURL[[x]]))
+matchURL <- lapply(seq(matchURL), function(x) unique(matchURL[[x]]))
 
 # Removing the first 4 unwanted URls from each nested list 
-macthURL <- lapply(1:69, function(x) tempMatchURL2[[x]][-1:-4])
+matchURL <- lapply(seq(matchURL), function(x) matchURL[[x]][-1:-4])
 
-# Adding the individual URL's to their respective matches in matchList
-matchList <- lapply(1:69, function(x){cbind(matchList[[x]], matchURL[[x]])})
+#Adding the text "http://stats.cricinfo.com" to complete the URL's
+matchURL <- lapply(seq(matchURL), function(x) paste("http://stats.cricinfo.com", matchURL[[x]], sep = ""))
+
+# Adding the individual URL's, Year and Month as a column to their respective matches in matchList
+matchYear <- lapply(seq(matchList), function(x) rep(names(matchList[x]),nrow(matchList[[x]])))
+matchMonth <- lapply(seq(matchList), function(x) substr(matchList[[x]][[6]],1,3))
+matchList <- lapply(seq(matchURL), function(x){cbind(matchList[[x]], matchURL[[x]],matchYear[[x]], matchMonth[[x]])})
+
+# Renaming the matchURL and matchYear column in the list elements
+matchLabel <- colnames(matchList[[1]])
+matchLabel[8] <- "MatchURL"
+matchLabel[9] <- "MatchYear"
+matchLabel[10] <- "MatchMonth"
+matchList <- lapply(seq(matchList), function(x)
+        { y <- matchList[[x]]
+          colnames(y) <- matchLabel
+          return(y)
+        })
 
 # Converting matchList to data frame for convinience
 tempDF1 <- data.frame()
 for (i in 1:length(matchList))
 {
-  tempDF2 <- matchTable[[i]]
+  tempDF2 <- matchList[[i]]
   matchTable <- rbind(tempDF1,tempDF2)
-  tempDF1 <- tempDF
+  tempDF1 <- matchTable
 }
+
+# Exporting the data frame to a XLSX file in the working directory
+write.xlsx(matchTable, "./IndiaCricRecords.xlsx", row.names = FALSE)
